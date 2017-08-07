9be38d07e8fcb8f85d79efe070774bae219f3a09
Murai <murai-y@7bunnies.com>
2017-08-08 00:40:18 +0900

    no message

---
+++ bba/asset/test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="test" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="test.png" trans="000000" width="1024" height="1024"/>
+</tileset>
